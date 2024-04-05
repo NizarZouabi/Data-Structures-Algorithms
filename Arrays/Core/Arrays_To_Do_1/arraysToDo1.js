a2bbd93de17d98e94147914209e986e1ddc4d933
NizarZouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-04-05 08:34:07 +0100

    #

---
+++ Arrays/Core/Arrays_To_Do_1/arraysToDo1.js
@@ -0,0 +1,81 @@
+//Push Front
+function pushFront(arr, value) {
+    const newArr = [value, ...arr]
+    return newArr
+}
+
+function pushFront1(arr, value) {
+    arr.unshift(value)
+    return arr
+}
+
+function pushFront2(arr, value) {
+    arr.push(arr[arr.length-1])
+    for(i=arr.length-1; i>0; i--) {
+        arr[i] = arr[i-1]
+    }
+    arr[0] = value
+    return arr
+}
+
+console.log(pushFront([2,3,4,5], 1));
+console.log(pushFront1([2,3,4,5], 1));
+console.log(pushFront2([2,3,4,5], 1));
+
+//Pop Front
+function popFront(arr) {
+    let arrRemovedVal = arr.shift()
+    console.log(arr)
+    return arrRemovedVal
+}
+
+function popFront1(arr) {
+    arr.reverse()
+    let arrRemovedVal = arr.pop()
+    arr.reverse()
+    console.log(arr)
+    return arrRemovedVal
+}
+
+function popFront2(arr) {
+    let arrRemovedVal = arr[0]
+    for (i=0; i<arr.length-1; i++) {
+        arr[i] = arr[i+1]
+    }
+    arr.length = arr.length-1 
+    console.log(arr)
+    return arrRemovedVal
+}
+
+console.log(popFront([0,5,10,15]));
+console.log(popFront1([0,5,10,15]));
+console.log(popFront2([0,5,10,15]));
+
+
+//Insert At
+
+function insertAt (arr, index, value) {
+    for (i = arr.length; i > index; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[i] = value;
+    return arr
+}
+
+function insertAt1 (arr, index, value) {
+    arr.splice(index, 0, value)
+    return arr
+}
+
+console.log(insertAt([2,5,6,8,14],1,3))
+console.log(insertAt1([2,5,6,8,14],1,3))
+
+
+//Remove At
+
+
+//Swap Pairs
+
+
+//Remove Duplicates
