551d30bf01e8ad0325da0aaba2d0c7f8bcda6e34
NizarZouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-04-17 20:12:00 +0100

    #

--- Strings & Associative Arrays/Core/Strings_To_Do_1/stringsToDo1.js
+++ Strings & Associative Arrays/Core/Strings_To_Do_1/stringsToDo1.js
@@ -17,6 +17,13 @@ console.log(removeBlanks("I can not BELIEVE it's not BUTTER"))
 //Get Digits
 
 function getDigits(str) {
+    let onlyDigits = Number();
+    for(i=0;i<str.length;i++){
+        if(!isNaN(str[i])){
+            onlyDigits += str[i]
+        }
+    }
+    return onlyDigits-0
 }
 
 console.log(getDigits("abc8c0d1ngd0j0!8"))
@@ -24,27 +31,44 @@ console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"))
 
 //Acronyms
 
-function acronyms() {
-
+function acronyms(word) {
+    let acroWord = word.split(" ");
+    let newWord = ""
+    for (i=0;i<acroWord.length;i++){
+        newWord += acroWord[i][0].toUpperCase()
+    }
+    return newWord;
 }
 
-console.log(acronyms())
-console.log(acronyms())
+console.log(acronyms("there's no free lunch - gotta pay yer way."))
+console.log(acronyms("Live from New York, it's Saturday Night!"))
 
 //Count Non-Spaces
 
-function countNonSpaces() {
-
+function countNonSpaces(str) {
+    let count = 0
+    for (i=0;i<str.length;i++){
+        if(str[i] != " "){
+            count++
+        }
+    }
+    return count;
 }
 
-console.log(countNonSpaces())
-console.log(countNonSpaces())
+console.log(countNonSpaces("Honey pie, you are driving me crazy"))
+console.log(countNonSpaces("Hello world !"))
 
 //Remove Shorter Strings
 
-function removeShorterStrings() {
-
+function removeShorterStrings(arr, val) {
+    let newArr = []
+    for(i=0;i<arr.length;i++){
+        if(arr[i].length >= val){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
 }
 
-console.log(removeShorterStrings())
-console.log(removeShorterStrings())
+console.log(removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4))
+console.log(removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3))
