99f254dcffed409607228af0cd07f0ecd0f417a5
NizarZouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-04-21 20:19:37 +0100

    #

---
+++ Singly Linked Lists/Practice/Max_Min_Average/minMaxAvg.js
@@ -0,0 +1,75 @@
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertAtEnd(val) {
+        let new_node = new Node(val)
+        if (this.head === null){
+            this.head = new_node;
+        } else {
+            let current = this.head
+            while(current.next){
+                current = current.next
+            }
+            current.next = new_node;
+        }
+    }
+
+    max(){
+        let current = this.head
+        let max = this.head.data
+        while(current && current !== null){
+            if(current.data > max){
+                max = current.data
+            }
+            current = current.next
+        }
+        return max
+    }
+
+    min(){
+        let current = this.head
+        let min = this.head.data
+        while(current && current !== null){
+            if(current.data < min){
+                min = current.data
+            }
+            current = current.next
+        }
+        return min
+    }
+
+    avg(){
+        let current = this.head
+        let avg = this.head.data
+        let min = this.min()
+        let max = this.max()
+        while(current && current !== null){
+            if(current.data > min && current.data < max){
+                avg = current.data
+            }
+            current = current.next
+        }
+        return avg
+    }
+
+
+
+}
+
+let SLL = new LinkedList()
+SLL.insertAtEnd(120)
+SLL.insertAtEnd(77)
+SLL.insertAtEnd(20)
+console.log("Largest value is: "+ SLL.max())
+console.log("Smallest value is: "+ SLL.min())
+console.log("Average value is: "+ SLL.avg())
+console.log(SLL)
