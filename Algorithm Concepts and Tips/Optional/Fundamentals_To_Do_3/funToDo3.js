1aed4447bc49f5fb5752480f7b24c417c494412b
NizarZouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-05-26 02:58:30 +0100

    #

--- Algorithm Concepts and Tips/Optional/Fundamentals_To_Do_3/funToDo3.js
+++ Algorithm Concepts and Tips/Optional/Fundamentals_To_Do_3/funToDo3.js
@@ -123,10 +123,11 @@ incTheSec([1,2,3,4,5,6])
 // Previous Lengths
 
 function prevLen(arr){
-    strLen = 0
-    for(let i=0;i<arr.length;i++){
-        strLen = arr[i].length
-        arr[i-1] = strLen
+    let prevlen = arr[0].length
+    for(i=1;i<arr.length;i++){
+        let temp = arr[i]
+        arr[i] = prevlen
+        prevlen = temp.length
     }
     console.log(arr)
 }
@@ -135,14 +136,91 @@ prevLen(["string","number","object","boolean","variable"])
 
 // Add Seven to Most
 
+function sevenToMost(arr){
+    newArr = []
+    let newArrIdx = 0
 
+    for(let i=1;i<arr.length;i++){
+        newArr[newArrIdx++] = arr[i]
+        newArr[newArrIdx++] = 7
+    }
+    console.log(newArr)
+}
+
+sevenToMost(['cats','lives','charCodes','kaiju'])
 
 // Reverse Array
 
+function reverseArr(arr){
+    let endIdx = arr.length-1
+
+    for(let i=0;i<arr.length/2;i++){
+        let temp = arr[i]
+        arr[i] = arr[endIdx]
+        arr[endIdx] = temp
+        endIdx--
+    }
+    console.log(arr)
+}
+
+reverseArr([1,2,3,4,5])
+
 // Outlook: Negative
+
+function outlookNegative(arr){
+    newArr = []
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] > 0){
+            arr[i] = arr[i] - arr[i] * 2
+        }
+        newArr.push(arr[i])
+    }
+    console.log(newArr)
+}
+
+outlookNegative([-6,2,-3,7])
 
 // Always Hungry
 
+function alwaysHungry(arr){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] === "food"){
+            console.log("yummy")
+        }
+    }
+    if (!arr.includes("food")){
+        console.log("i'm hungry")
+    }
+}
+
+alwaysHungry(["no food","no food","food"])
+
 // Swap Toward the Center
 
+function swap(arr){
+    let lastIdx = arr.length-1
+
+    for(let i=0;i<arr.length/2;i+=2){
+        let temp = arr[i]
+        arr[i] = arr[lastIdx]
+        arr[lastIdx] = temp
+
+        lastIdx-=2
+    }
+    console.log(arr)
+}
+
+swap([1,2,3,4,5,6])
+
 // Scale the Array
+
+function scaleArr(arr, num){
+    for(i=0;i<arr.length;i++){
+        arr[i] *= num
+    }
+
+    return console.log(arr)
+}
+
+scaleArr([2,4,8,16], 2)
---
+++ Masterclass assignments/Frequency Counters/anagram.js
@@ -0,0 +1,35 @@
+const validAnagram = (str1, str2) => {
+    str1 = str1.toLowerCase()
+    str2 = str2.toLowerCase()
+
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+
+    if(str1.length !== str2.length){
+        return false
+    }
+
+    for(let val of str1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) +1
+    }
+    console.log(frequencyCounter1)
+    
+    for(let val of str2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) +1
+    }
+    console.log(frequencyCounter2)
+
+    for(let key in frequencyCounter1){
+        if(frequencyCounter1[key] !== frequencyCounter2[key]){
+            return false
+        }
+    }
+    return true
+}
+
+//no numbers, no spacing, only lowercase
+console.log(validAnagram('','')) //true
+console.log(validAnagram('Anagram','Nagrama')) //true
+console.log(validAnagram('rat','car')) //false
+console.log(validAnagram('tacocat','cattaco')) //true
+
