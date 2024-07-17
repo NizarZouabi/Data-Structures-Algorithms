b23f636a10a06d7dfb40b336133931ae068ec44f
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-07-17 22:00:46 +0100

    #

--- Masterclass assignments/Searching Algorithms/binarySearch.js
+++ Masterclass assignments/Searching Algorithms/binarySearch.js
@@ -1,23 +1,20 @@
 function binarySearch(arr, val){
     let left = 0
     let right = arr.length-1
-    while(left < right){
-        var mid = arr.length/2 -0.5
-        if(left < mid){
-            if(arr[left] === val){
-                return console.log(left)
-        }  else if (right > mid){
-            if(arr[right] === val){
-                return console.log(right)
-            } else if (arr[mid] === val){
-                return console.log(mid)
-            }
-            right--
+    while(arr[mid] !== val && left <= right){
+        var mid = Math.floor((left + right)/2)
+        if(val < arr[mid]){
+            right = mid-1
+        } else {
+            left = mid+1
         }
-        left++
     }
-}
-    return console.log(-1)
+
+    if(arr[mid] === val){
+        return console.log(mid)
+    } else {
+        return console.log(-1)
+    }
 }
 
 binarySearch([10,11,12,13,14,15,16,17,18,19,20,21,22,23,24], 17)
