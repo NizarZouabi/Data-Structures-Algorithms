68f13bec3b567b1046b1661d3875407726061715
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-07-21 00:16:30 +0100

    #

--- Masterclass assignments/Searching Algorithms/naiveStringSearch.js
+++ Masterclass assignments/Searching Algorithms/naiveStringSearch.js
@@ -1,14 +1,12 @@
 function naiveSearch(long, short){
     let matchCount = 0
-    let len = 0
     
     for(let i=0; i<long.length;i++){
         for(let j=0; j<short.length; j++){
-            if(long[i+j] !== short[j]){
+            if(long[i+j] === short[j]){
+                if (j === short.length-1) matchCount++
+            } else {
                 break
-            }
-            if (j === short.length-1){
-                matchCount++
             }
         }
     }
