dad5b614d263d14ea61b21a55112e9c9c9566035
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-07-25 23:53:29 +0100

    #

--- Algorithm Concepts and Tips/Core/funToDo4.js
+++ Algorithm Concepts and Tips/Core/funToDo4.js
@@ -13,7 +13,28 @@ keepLast([2,4,6,8,10],3)
 
 // Math Help
 
+function interceptX(M, B){
+    if(M === 0){
+        throw new Error("'M can't be zero'")
+    }
+    let X = -B / M
+    console.log(X)
+}
+
+interceptX(12, 6)
+
 // Poor Kenny
+
+function whatHappensToday(){
+    const disasters = ['volcanos', 'tsunami', 'earthquakes', 'blizzards', 'meteorites']
+    const probability = [0.1, 0.15, 0.2, 0.25, 0.3]
+
+    for(let i=0; i<=disasters.length-1; i++){
+        if(Math.random() < probability[i]) console.log(disasters[i])
+    }
+}
+
+whatHappensToday()
 
 // What Really Happened?
 
