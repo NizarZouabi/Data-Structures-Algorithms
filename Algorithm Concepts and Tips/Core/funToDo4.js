1d5e81b4c9f6771adf48c8c4d11d92561ee32069
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-07-31 22:01:17 +0100

    #

--- Algorithm Concepts and Tips/Core/funToDo4.js
+++ Algorithm Concepts and Tips/Core/funToDo4.js
@@ -28,18 +28,62 @@ interceptX(12, 6)
 function whatHappensToday(){
     const disasters = ['volcanos', 'tsunami', 'earthquakes', 'blizzards', 'meteorites']
     const probability = [0.1, 0.15, 0.2, 0.25, 0.3]
+    let cumulativeProbability = 0
+
+    for(let i=0; i<disasters.length; i++){
+        cumulativeProbability += probability[i]
+        if(Math.random() < cumulativeProbability){
+            return disasters[i]
+        }
+    }
+}
+
+console.log(whatHappensToday())
+
+// What Really Happened?
+
+function whatReallyHappensToday(){
+    const disasters = ['volcanos', 'tsunami', 'earthquakes', 'blizzards', 'meteorites']
+    const probability = [0.1, 0.15, 0.2, 0.25, 0.3]
 
     for(let i=0; i<=disasters.length-1; i++){
         if(Math.random() < probability[i]) console.log(disasters[i])
     }
 }
 
-whatHappensToday()
-
-// What Really Happened?
+whatReallyHappensToday()
 
 // Soaring IQ
 
+function soaringIQ(){
+    let IQ = 101
+    let days = 98
+    let inc = 0.01
+
+    for(let i=1; i<days; i++){
+        inc += 0.01
+        IQ += inc
+    }
+
+    return IQ
+}
+
+console.log(soaringIQ())
+
 // Letter Grade
 
-// More Accurate Grades
+function letterGrade(score){
+    if(score < 0 || score > 100) return 'score must be between 0 & 100'
+
+    if(score <= 100 && score >= 90) {
+        return `Score: ${score} Grade: A`
+    } else if(score <= 89 && score >= 80) {
+        return `Score: ${score} Grade: B`
+    } else if(score <= 79 && score >= 70){
+        return `Score: ${score} Grade: C`
+    } else if(score <= 69 && score >= 60){
+        return `Score: ${score} Grade: D`
+    } else return `Score: ${score} Grade: F`
+}
+
+console.log(letterGrade(90))
