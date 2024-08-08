7a501f57c5cf89de5bae837af9bf60575fb050d9
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-08-08 21:52:12 +0100

    #

--- Arrays/Core/arrToDo3.js
+++ Arrays/Core/arrToDo3.js
@@ -1,12 +1,68 @@
-// const rSigma = () => {
-//     var arr = [42,68,7,21,243,512];
-//     for (var x = arr.length-2; x > 1; x--) {
-//         arr[x - 1] = arr[x + 1];
-        
-//     }
-//     console.log(arr);
-//     console.log(x)
-// }
+// Remove Negatives
 
-// rSigma()
+function removeNegatives(arr){
+    let negativeNum = 0
 
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] >= 0){
+            [arr[negativeNum], arr[i]] = [arr[i], arr[negativeNum]]
+            negativeNum++
+        }
+    }
+
+    arr.length = negativeNum
+
+    console.log(arr)
+}
+
+removeNegatives([-2,-1,15,-8,6])
+
+// Second-to-Last
+
+function SecondToLast(arr){
+    if(arr.length < 2) return null
+    return console.log(arr[arr.length-2])
+}
+
+SecondToLast([42,true,4,"Kate",7])
+
+// Second-Largest
+
+function secondLargest(arr){
+    let secLargest = 0
+    let max = 0
+    let i = 0
+    
+    if(arr.length < 2) return null
+
+    while(i<arr.length){
+        if(arr[i] > max){
+            max = arr[i]
+        } else if(arr[i] > secLargest && arr[i] < max && arr[i] != max){
+            secLargest = arr[i]
+        }
+        i++
+    }
+
+    return console.log(secLargest)
+}
+
+secondLargest([42,1,4,Math.PI,7])
+
+// Nth-to-Last
+
+function nthToLast(){
+
+}
+
+// Nth-Largest
+
+function nthLargest(){
+
+}
+
+// Skyline Heights
+
+function skyLineHeights(){
+
+}
