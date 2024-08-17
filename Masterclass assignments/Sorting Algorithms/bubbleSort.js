e1745aa48b0e26b764e7ca39511866c527c2ab8b
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-08-17 04:33:33 +0100

    #

---
+++ Masterclass assignments/Sorting Algorithms/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr){
+    for(i=arr.length-1; i>=0; i--){
+        let j = 0
+        for(j=0; j<=i-1; j++){
+            if(arr[j] > arr[j+1]){
+                let swapper = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = swapper
+            }
+        }
+    }
+
+    return arr
+}
+
+console.log(bubbleSort([12,5,44,300,0]))
