21de7633d76e7a1bcaac34473813bccb989ca8c9
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-08-31 08:06:25 +0100

    #

--- Algo Book Challenges/Ch_2/popFront.js
+++ Algo Book Challenges/Ch_2/popFront.js
@@ -1,5 +1,13 @@
 function popFront(arr){
-    
+    let holder = arr[0]
+    for(i=0;i<arr.length;i++){
+        arr[i] = arr[i+1]
+    }
+    arr[arr.length-1] = holder
+    arr.length--
+
+    console.log(arr)
+    console.log(holder)
 }
 
 popFront([22,96,77])
