16481c69aaba3541d7091303adbab210325f7548
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-09-19 23:57:58 +0100

    #

---
+++ Algo Book Challenges/Ch_2/binarySearch.js
@@ -0,0 +1,23 @@
+function binarySearch(arr, val){
+    let left = 0
+    let right = arr.length-1
+    let notFound = true
+
+    while(left <= right){
+        let mid = Math.floor((left+right)/2)
+        if(arr[mid] === val){
+            notFound = false
+            return mid
+        } else if(val > arr[mid]) {
+            left = mid +1
+
+        } else if(val < arr[mid]){
+            right = mid -1
+        }
+    }
+
+    return -1
+}
+
+let orderedArr = [1,2,3,4,5,6,7,8,9]
+console.log(binarySearch(orderedArr, 7))
