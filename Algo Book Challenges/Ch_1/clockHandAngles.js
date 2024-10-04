7a434d0604dcaa151ae81410f3df44fd09c78716
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-10-04 21:25:39 +0100

    #

--- Algo Book Challenges/Ch_1/clockHandAngles.js
+++ Algo Book Challenges/Ch_1/clockHandAngles.js
@@ -1,5 +1,16 @@
 function clockHand(seconds){
-    
+
+    const secs = seconds % 60
+    const mins = Math.floor((seconds % 3600) / 60)
+    const hours = Math.floor(seconds / 3600)
+
+
+    let secsDegree = secs * 6
+    let minsDegree = mins * 6
+    let hoursDegree = (hours % 12) * 30
+
+    console.log(hours ,mins, secs)
+    console.log(`HoursHand: ${hoursDegree}, MinutesHand: ${minsDegree}, SecondsHand: ${secsDegree}`)
 }
 
-clockHand()
+clockHand(43160)
